2701bd44220b12911002ed8d5f0632240f9c79b9
lelikdark <dark@darkbsd.net>
2015-08-31 12:51:10 +0300

    +

--- js/main.js
+++ js/main.js
@@ -4,5 +4,18 @@ $(function() {
 
 $('.ui.dropdown').dropdown();
 
+$('.browse')
+    .popup({
+        inline   : true,
+        hoverable: true,
+        position : 'bottom left',
+        delay: {
+            show: 300,
+            hide: 800
+        }
+    })
+;
+
+
 
 
